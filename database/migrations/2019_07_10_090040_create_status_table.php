21f937813fdafd60619781324613a3ffd9d32d0b
haniivn <ivana_putri_25rpl@student.smktelkom-mlg.sch.id>
2019-07-10 16:15:08 +0700

    table comments dan status

---
+++ database/migrations/2019_07_10_090040_create_status_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->text('text')->nullable();
+            $table->text('picture')->nullable();
+            $table->text('document')->nullable();
+            $table->text('video')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status');
+    }
+}
